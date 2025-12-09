ee4dfd83510981082c14ff9dbf2c443d8186a32f
Dewan Mahin Chowdhury <67519352+dewanmahin@users.noreply.github.com>
2025-12-10 01:41:08 +0600

    Create CategoryComponent.java

---
+++ src/main/java/org/example/tools/CategoryComponent.java
@@ -0,0 +1,10 @@
+package org.example.tools;
+
+public abstract class CategoryComponent {
+    public void add(CategoryComponent component) {
+        throw new UnsupportedOperationException();
+    }
+    public double getAmount() {
+        throw new UnsupportedOperationException();
+    }
+}
